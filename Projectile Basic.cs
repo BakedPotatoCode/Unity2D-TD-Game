7e865ff78312d58fb5e9a8207ba98e9f8e1c94a7
BakedPotatoCode <160190823+BakedPotatoCode@users.noreply.github.com>
2024-02-15 16:02:36 -0800

    Update Projectile Basic.cs

--- Projectile Basic.cs
+++ Projectile Basic.cs
@@ -4,10 +4,25 @@ public class Projectile : MonoBehaviour
 {
     public float speed = 20f;
     public int damage = 50;
+    public float lifetime = 2f; // Lifetime of the projectile in seconds
+
+    private float timer;
+
+    void Start()
+    {
+        timer = lifetime;
+    }
 
     void Update()
     {
         transform.Translate(Vector3.up * speed * Time.deltaTime);
+        // Decrease the timer
+        timer -= Time.deltaTime;
+        // Destroy the projectile if the timer reaches zero
+        if (timer <= 0f)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
